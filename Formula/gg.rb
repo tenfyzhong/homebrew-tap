ad564666cf05196c098cef96703f786d37935cf2
zhongtenghui <zhongtenghui@bytedance.com>
2024-01-27 17:58:14 +0800

    fix ci

--- Formula/gg.rb
+++ Formula/gg.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Gg < Formula
   desc "Golang version manager"
   homepage "https://github.com/tenfyzhong/gg"
