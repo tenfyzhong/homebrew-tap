6dfb2621ed6797eb2008a012ed51e39309b169f2
zhongtenghui <zhongtenghui@bytedance.com>
2024-01-27 18:00:12 +0800

    gg2

---
+++ Formula/gg2.rb
@@ -0,0 +1,32 @@
+class Gg < Formula
+  desc "Golang version manager"
+  homepage "https://github.com/tenfyzhong/gg"
+  url "https://github.com/tenfyzhong/gg/archive/refs/tags/1.0.1.tar.gz"
+  sha256 "115cc675c77950c62d33e86e608d51e96b4f610c8968c06512172a3c85c0ad27"
+  license "MIT"
+
+  depends_on "curl"
+
+  def install
+    bin.install "gg"
+    bash_completion.install "gg-completion.bash" => "gg"
+    zsh_completion.install "_gg" => "_gg"
+  end
+
+  test do
+    output = shell_output("#{bin}/gg -h")
+    assert_equal "gg: golang version manager
+Usage: gg [options] <subcommand> [options] <args>
+
+Options:
+  -h/--help         print this help message
+
+Subcommands:
+  ls           list local version
+  ls-remote    list remote version
+  install      install specified version
+  remove       remove specified version
+  use          print the specified version environment
+", output
+  end
+end
