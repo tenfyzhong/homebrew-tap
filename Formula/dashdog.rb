29605a83a0bd1646f503567772c17bb1cc5a2850
tenfy <tenfy@tenfy.cn>
2024-05-05 18:26:01 +0800

    dashdog 0.1.4 (#20)
    * dashdog 0.1.4
    
    * feat(dashdog): fix completion

--- Formula/dashdog.rb
+++ Formula/dashdog.rb
@@ -1,8 +1,8 @@
 class Dashdog < Formula
   desc "dashdog is a tool to generate docset for [dash](https://kapeli.com/dash)"
   homepage "https://github.com/tenfyzhong/dashdog"
-  url "https://github.com/tenfyzhong/dashdog/archive/refs/tags/0.1.3.tar.gz"
-  sha256 "3f5531d35e71d1d941e1ccfcaabc597441eb0b1a94bbfe5e5bf992f68a32efc3"
+  url "https://github.com/tenfyzhong/dashdog/archive/refs/tags/0.1.4.tar.gz"
+  sha256 "8bed83b506fcdc245c3b1416f148409f59db1031559f912e0ffc811bd093da36"
   license "MIT"
 
   depends_on "go" => :build
@@ -10,12 +10,12 @@ class Dashdog < Formula
   def install
     system "go", "build", "-ldflags", "-X 'github.com/tenfyzhong/dashdog/cmd/dashdog/version.Version=#{version}'", "-o", bin/"dashdog", "./cmd/dashdog"
     bash_completion.install "cmd/dashdog/completions/dashdog.bash" => "dashdog"
-    zsh_completion.install "cmd/dashdog/completions/_dashdog" => "dashdog"
+    zsh_completion.install "cmd/dashdog/completions/_dashdog" => "_dashdog"
     fish_completion.install "cmd/dashdog/completions/dashdog.fish" => "dashdog.fish"
 
     bin.install "cmd/dashdog-go/dashdog-go"
     bash_completion.install "cmd/dashdog-go/completions/dashdog-go.bash" => "dashdog-go"
-    zsh_completion.install "cmd/dashdog-go/completions/_dashdog-go" => "dashdog-go"
+    zsh_completion.install "cmd/dashdog-go/completions/_dashdog-go" => "_dashdog-go"
     fish_completion.install "cmd/dashdog-go/completions/dashdog-go.fish" => "dashdog-go.fish"
 
     pkgshare.install "conf"
@@ -30,7 +30,7 @@ USAGE:
    dashdog -c|--config <file> [--log off] [config options]
 
 VERSION:
-   dev
+   #{version}
 
 AUTHOR:
    tenfyzhong
