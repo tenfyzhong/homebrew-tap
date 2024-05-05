26f93930117b7cad192f1d48496feb0c2a14510b
tenfy <tenfy@tenfy.cn>
2024-05-05 10:21:37 +0000

    dashdog 0.1.4

--- Formula/dashdog.rb
+++ Formula/dashdog.rb
@@ -1,8 +1,8 @@
 class Dashdog < Formula
   desc "dashdog is a tool to generate docset for [dash](https://kapeli.com/dash)"
   homepage "https://github.com/tenfyzhong/dashdog"
-  url "https://github.com/tenfyzhong/dashdog/archive/refs/tags/0.1.3.tar.gz"
-  sha256 "3f5531d35e71d1d941e1ccfcaabc597441eb0b1a94bbfe5e5bf992f68a32efc3"
+  url "https://github.com/tenfyzhong/dashdog/archive/refs/tags/0.1.4.tar.gz"
+  sha256 "8bed83b506fcdc245c3b1416f148409f59db1031559f912e0ffc811bd093da36"
   license "MIT"
 
   depends_on "go" => :build
