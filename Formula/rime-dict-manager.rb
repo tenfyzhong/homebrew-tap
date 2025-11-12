c0702b5a5d2d1b050200075d6b19b231ae2ffdae
Tenghui Zhong <tenfy@tenfy.cn>
2025-11-12 13:54:05 +0000

    rime-dict-manager 0.1.1

--- Formula/rime-dict-manager.rb
+++ Formula/rime-dict-manager.rb
@@ -4,8 +4,8 @@
 class RimeDictManager < Formula
   desc "一个用于管理 [Rime](https://rime.im/) 用户词典的命令行工具."
   homepage "https://github.com/tenfyzhong/rime-dict-manager"
-  url "https://github.com/tenfyzhong/rime-dict-manager/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "abe7bcb941f34b4515b6767952a3dd781cdd3cc5ffaf8cdce874973c2c7dd72d"
+  url "https://github.com/tenfyzhong/rime-dict-manager/archive/refs/tags/0.1.1.tar.gz"
+  sha256 "d185831827d5a1c41f90defb6334df2c98b6d6bbff3515163fb2dad9924c7be8"
   license "MIT"
 
   depends_on "go" => :build
