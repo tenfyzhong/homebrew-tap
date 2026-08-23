6acfbf4d5c82bba5fbc6aa46549892fdccdbd00e
tenfyzhong <5044825+tenfyzhong@users.noreply.github.com>
2026-08-23 07:45:25 +0000

    modeltap 0.2.10

--- Formula/modeltap.rb
+++ Formula/modeltap.rb
@@ -1,13 +1,13 @@
 class Modeltap < Formula
   desc "MITM AI traffic monitor with configurable egress proxies"
   homepage "https://github.com/tenfyzhong/modeltap"
-  url "https://github.com/tenfyzhong/modeltap/archive/refs/tags/0.2.9.tar.gz"
-  sha256 "e550401ae253ed60bf9d1b238c69b42dc8f36e15e881d46c48acaf5c99590644"
+  url "https://github.com/tenfyzhong/modeltap/archive/refs/tags/0.2.10.tar.gz"
+  sha256 "2bf4007510d784e8343b93ea2f1bbe778a832603db652d62bc05326f45c3fd13"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/tenfyzhong/modeltap/releases/download/0.2.9"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "f78be2871ff422a7ee58c1dec033b52833fb88ee1ad7ec4ee7050d021d119244"
+    root_url "https://github.com/tenfyzhong/modeltap/releases/download/0.2.10"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "7eaced30505b6a8240286c830908d319b36e6af99d93b185aafec3b20be2a426"
   end
 
   depends_on "rust" => :build
