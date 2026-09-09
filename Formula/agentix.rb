c2bb06f899cea41c1b82e487a5c6cf79bcf6f668
tenfyzhong <5044825+tenfyzhong@users.noreply.github.com>
2026-09-09 17:27:38 +0000

    agentix 0.3.0
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/agentix.rb
+++ Formula/agentix.rb
@@ -1,13 +1,13 @@
 class Agentix < Formula
   desc "Control local coding-agent sessions from IM"
   homepage "https://github.com/tenfyzhong/agentix"
-  url "https://github.com/tenfyzhong/agentix/archive/refs/tags/0.2.4.tar.gz"
-  sha256 "f0d976fab3bd1b86f0951f7431caa829c34b730a4bf1b6ce4208aee3520096e5"
+  url "https://github.com/tenfyzhong/agentix/archive/refs/tags/0.3.0.tar.gz"
+  sha256 "6cdad285121943e2b6ac3504102c81a9e77e391f678e43d8af55651ae50d2bd8"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/tenfyzhong/agentix/releases/download/0.2.4"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7fd39134f0ca73e491f55b88f0c026ef6a0be9868f2591bfaea5f2db30483779"
+    root_url "https://github.com/tenfyzhong/agentix/releases/download/0.3.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "45f368f9038694f67a3dbb29300e9b062a4574d7aa2bd57570abe7ca1620658b"
   end
 
   depends_on "protobuf" => :build
